6343c9ffab4fdb968ab2bc800648ca5378e79728
cindy <1177031+cindywu@users.noreply.github.com>
2023-02-21 21:43:24 -1000

    again

--- pages/api/north-shore.ts
+++ pages/api/north-shore.ts
@@ -1,16 +1,23 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler (_req: NextApiRequest, res: NextApiResponse) {
-  const response = await fetch('https://services.surfline.com/kbyg/regions/forecasts/conditions?subregionId=58581a836630e24c44878fcb&days=2',
+  try {
+    const response = await fetch('https://services.surfline.com/kbyg/regions/forecasts/conditions?subregionId=58581a836630e24c44878fcb&days=2',
   {
     method: 'GET',
     headers: {
       'Content-Type': 'application/json',
     },
   });
+  console.log('response', response)
 
   const json = await response.json();
   res.status(200).json(json);
+
+  } catch(error) {
+    console.log('error', error)
+  }
+
 };
 
 
