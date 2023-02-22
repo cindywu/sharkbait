084dee2ad71f8c485b0504530f541e4d9243028c
cindy <1177031+cindywu@users.noreply.github.com>
2023-02-21 16:26:19 -1000

    make it work

---
+++ app/page.tsx
@@ -0,0 +1,53 @@
+"use client"
+import React from 'react'
+import useSWR from 'swr'
+
+const ssFetcher = () => fetch('/api/south-shore').then((res) => res.json())
+const nsFetcher = () => fetch('/api/north-shore').then((res) => res.json())
+const wsFetcher = () => fetch('/api/west-side').then((res) => res.json())
+
+export default function Home() {
+  const {
+    data: ssData,
+    error: ssError,
+    isLoading: ssIsLoading,
+  } = useSWR('/api/south-shore', ssFetcher)
+
+  const {
+    data: nsData,
+    error: nsError,
+    isLoading: nsIsLoading,
+   } = useSWR('/api/north-shore', nsFetcher)
+
+   const {
+    data: wsData,
+    error: wsError,
+    isLoading: wsIsLoading
+   } = useSWR('api/west-side', wsFetcher)
+
+
+  return (
+    <>
+      surfbot
+      {ssData && <SurfStuff subRegionName={'south shore'} data={ssData}/>}
+      {nsData && <SurfStuff subRegionName={'north shore'} data={nsData}/>}
+      {wsData && <SurfStuff subRegionName={'west side'} data={wsData}/>}
+    </>
+  )
+}
+
+function SurfStuff({subRegionName, data}: any) {
+  return (
+    <div>
+      <div>
+        {subRegionName}
+        <div>
+          {data.data.conditions[0].am.minHeight}
+          -
+          {data.data.conditions[0].am.maxHeight}
+        </div>
+      </div>
+    </div>
+  )
+}
+
---
+++ pages/api/north-shore.ts
@@ -0,0 +1,10 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async(_req: NextApiRequest, res: NextApiResponse) => {
+  const response = await fetch('https://services.surfline.com/kbyg/regions/forecasts/conditions?subregionId=58581a836630e24c44878fcb&days=2');
+
+  const json = await response.json();
+  res.status(200).json(json);
+};
+
+
